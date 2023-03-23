eb14715987d53bbf15f911b10d3248410b85bf14
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-23 16:18:30 +0000

    setup config for chat component and import using @ symbol

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Chat from "@/components/chat"
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/chat" element={<chat/>} />
+          <Route path="/chat" element={<Chat/>} />
         </Routes>
       </BrowserRouter>    
 
