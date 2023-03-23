e9b8fa2b1c4b9509c8f9090e3fc5f1e4fba6e085
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-23 16:42:25 +0000

    create chat component

---
+++ client/src/components/customHeader/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid"
+import Chat from '../chat'
+
+const CustomHeader = ( {chat} ) => {
+  return (
+    <div className="chat-header">
+        <div className="flexbetween">
+            <ChatBubbleLeftRightIcon className="icon-chat" />
+            <h3 className="header-test">{ Chat.title }</h3>
+        </div>
+
+        <div className="flexbetween">
+            <PhoneIcon className="icon-phone" />
+            <p className="header-text">{ chat.description }</p>
+        </div>
+    </div>
+  )
+}
+
+export default CustomHeader
